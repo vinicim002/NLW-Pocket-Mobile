e4e31502083185abef1477f99b33cc64061f03b4
Vinicius <104523463+vinicim002@users.noreply.github.com>
2024-12-10 13:27:46 -0300

    criando arquivos
    Criei a pasta src e dentro dela criei uma pasta app onde criei os arquivos _layout.tsx e index.tsx

---
+++ mobile/src/app/index.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native"
+
+export default function Index(){
+    return (
+        <View 
+            style={{
+                flex: 1, 
+                justifyContent: "center",
+                alignItems: "center"
+            }}
+        >
+            <Text style={{fontSize: 22}}>Hello React Native</Text>
+        </View>
+    )
+}
